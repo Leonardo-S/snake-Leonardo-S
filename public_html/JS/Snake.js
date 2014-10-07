3e2607c9499285306bee33d19b5467f4543a8cf9
Leonardo Sanchez <leonardodicaprio831@yahoo.com>
2014-10-07 08:20:41 -0700

    i have started my snake project and added variables

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -1,17 +1,48 @@
 var snake;
+var snakelength;
+var snakesize;
 
 var context;
 var screenwidth;
-var scrrenheight;
+var screenheight;
+gameInitialize();
+gameDraw();
 
-function gameinitialize() {
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen"); 
+    context = canvas.getContext("2d");
+    
+    screenwidth = window.innerWidth;
+    screenheight = window.innerHeight;
+    
+    canvas.width = screenwidth;
+    canvas.height = screenheight;
+}
+
+function gameLoop () {
     
 }
 
-function gameloop () {
+function gameDraw() {
+    context.fillStyle ="rgb(47,142,222)";
+    context.fillRect(0,0,screenwidth,screenheight);
+}
+
+function snakeInitialize(){
+    snake = [];
+    snakelength = 5;
+    snakesize = 20;
+    for(var index = 0; index < sankeLength; ++){
+        snake.push();
+        
+    }
+  
+}
+
+function sankeDraw(){
     
 }
 
-function gamedraw() {
+function snakeUpdate(){
     
 }
