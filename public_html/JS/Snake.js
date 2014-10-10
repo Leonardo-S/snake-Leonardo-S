7c47c3d8f0cb95af7cf7b8dcedb00fc76e5c43dd
Leonardo Sanchez <leonardodicaprio831@yahoo.com>
2014-10-09 23:11:41 -0700

    i have added new elements to my snake

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -8,9 +8,7 @@ var screenHeight;
 
 gameInitialize();
 snakeInitialize();
-gameDraw();
-snakeDraw();
-
+gameLoop();
 function gameInitialize() {
     var canvas = document.getElementById("game-screen"); 
     context = canvas.getContext("2d");
@@ -23,18 +21,21 @@ function gameInitialize() {
 }
 
 function gameLoop () {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
 }
 
 function gameDraw() {
-    context.fillStyle ="rgb(47,142,222)";
+    context.fillStyle ="rgb(37,245,0)";
     context.fillRect(0,0,screenWidth,screenHeight);
+    snakeDraw();
 }
 
 function snakeInitialize(){
     snake = [];
     snakeLength = 10;
-    snakeSize = 20;
+    snakeSize = 15;
     for(var index = 0; index < snakeLength; index++){
         snake.push({
             x: index,
@@ -46,12 +47,17 @@ function snakeInitialize(){
 
 function snakeDraw(){
    for(var index = 0; index < snake.length; index++){
-       context.fillStyle = "white";
+       context.fillStyle = "red";
        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize,snakeSize,snakeSize);
    }
     }
 
 
 function snakeUpdate(){
-    var snakeHeadx
-}
+    var snakeHeadx = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+   }
